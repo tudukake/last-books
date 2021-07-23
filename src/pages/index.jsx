e8b19b864e3d236e070c3ebf452f43f9aabdf3aa
tudukake <info@tudukake.com>
2021-07-23 13:29:49 +0900

    ✨ supabaseをライブラリ化

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,28 +4,37 @@ import { client } from 'src/libs/supabase';
 
 const Container = (props) => {
   const { user } = Auth.useUser();
+
+  // ログインしている場合
   if (user) {
     return (
       <>
-        <Typography.Text>Signed in: {user.email}</Typography.Text>
-        <Button block onClick={() => props.supabaseClient.auth.signOut()}>
-          Sign out
+        <Typography.Text>login: {user.email}</Typography.Text>
+        <Button block onClick={() => client.auth.signOut()}>
+          Sign Out
         </Button>
       </>
     );
   }
+
+  // ログインしていない場合
   return props.children;
 };
 
-export default function AuthBasic() {
+const Home = () => {
   return (
     <LayoutWrapper>
-        <Auth
-          supabaseClient={supabase}
-          providers={['google', 'facebook', 'github']}
-        />
-      </Container>
-    </Auth.UserContextProvider>
+      <Auth.UserContextProvider supabaseClient={client}>
+        <Container>
+          <Auth
+            supabaseClient={client}
+            providers={['google', 'facebook', 'github']}
+            socialColors={false}
+          />
+        </Container>
+      </Auth.UserContextProvider>
     </LayoutWrapper>
   );
-}
+};
+
+export default Home;
