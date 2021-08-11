f059a14b3b6c8107fe9d47536653b6ed8c9df18f
tudukake <info@tudukake.com>
2021-08-11 23:36:51 +0900

    ✨ Twitter認証

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -37,7 +37,7 @@ const Home = () => {
         <Container>
           <Auth
             supabaseClient={client}
-            providers={['github']}
+            providers={['twitter', 'github']}
             socialColors={true}
           />
         </Container>
