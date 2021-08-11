438eeb6a414948bc92c80931d69b93a44d45065d
tudukake <info@tudukake.com>
2021-08-11 16:49:51 +0900

    ✨ 説明を追加

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,14 +6,17 @@ import logo from 'public/logo.png';
 export const Header = () => {
   return (
     <header className={style.header}>
-      <Link href='/'>
-        <a>
-          <Image src={logo} alt='logo image' width={45} height={45} />
-        </a>
-      </Link>
-      <Link href='/'>
-        <a className={style.logo}>最後の本</a>
-      </Link>
+      <div className={style.box_logo}>
+        <Link href='/'>
+          <a>
+            <Image src={logo} alt='logo image' width={40} height={36} />
+          </a>
+        </Link>
+        <Link href='/'>
+          <a className={style.logo}>最後の本</a>
+        </Link>
+      </div>
+      <div className={style.description}>～ 同本再購入防止 ～</div>
     </header>
   );
 };
