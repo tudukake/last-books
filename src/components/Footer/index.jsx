f9f5993759a5baa52ad814f28e4466e56f8bdadd
tudukake <info@tudukake.com>
2021-08-15 15:03:12 +0900

    ♻️ コピーライトを変更

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import style from 'src/components/Footer/Footer.module.css';
 export const Footer = () => {
   return (
     <footer className={style.footer}>
-      <address className={style.address}>&copy; 2021 tudukake.com</address>
+      <address className={style.address}>&copy; 2021 last-books</address>
     </footer>
   );
 };
